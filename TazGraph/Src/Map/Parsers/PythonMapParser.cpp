3cf825af4e549baec12a94cdf1a6baca9f90e81d
Mujin <lefteriskotsonas2@gmail.com>
2025-07-30 16:53:46 +0300

    multithread python parse

--- TazGraph/Src/Map/Parsers/PythonMapParser.cpp
+++ TazGraph/Src/Map/Parsers/PythonMapParser.cpp
@@ -8,46 +8,117 @@ void PythonMapParser::parse(Manager& manager,
 	std::function<void(Entity&)> addLinkFunc) 
 {
 
-	glm::vec2 minPos(FLT_MAX);
-	glm::vec2 maxPos(FLT_MIN);
-
 	JsonParser fileParser(file);
 	JsonValue rootFromFile = fileParser.parse();
 	auto& nodes = rootFromFile.obj["graph"].obj["nodes"];
-
-	for (auto& nodeEntry : nodes.obj) {
-		int nodeId = std::stoi(nodeEntry.first);
-		auto& nodeInfo = nodeEntry.second;
-		float x = nodeInfo.obj["metadata"].obj["x"].num / 10.0f;
-		float y = nodeInfo.obj["metadata"].obj["y"].num / 10.0f;
-
-		auto& node = manager.addEntity<Node>();
-		glm::vec3 position(x, y, 0);
-
-#if defined(_WIN32) || defined(_WIN64)
-		minPos.x = std::min(minPos.x, x);
-		minPos.y = std::min(minPos.y, y);
-		maxPos.x = std::max(maxPos.x, x);
-		maxPos.y = std::max(maxPos.y, y);
-#else
-		minPos.x = std::min(minPos.x, x);
-		minPos.y = std::min(minPos.y, y);
-		maxPos.x = std::max(maxPos.x, x);
-		maxPos.y = std::max(maxPos.y, y);
-#endif
-
-		addNodeFunc(node, position);
-	}
 	auto& links = rootFromFile.obj["graph"].obj["edges"];
-	for (auto& linkEntry : links.arr) {
-		auto& linkInfo = linkEntry;
-		unsigned int fromID = linkInfo.obj["source"].num;
-		unsigned int toID = linkInfo.obj["target"].num;
 
-		auto& link = manager.addEntity<Link>(fromID, toID);
-		addLinkFunc(link);
+	std::vector<std::pair<std::string, JsonValue*>> nodeEntries;
+	nodeEntries.reserve(nodes.obj.size());
+	for (auto& nodeEntry : nodes.obj) {
+		nodeEntries.emplace_back(nodeEntry.first, &nodeEntry.second);
 	}
 
+	std::vector<JsonValue*> linkEntries;
+	linkEntries.reserve(links.arr.size());
+	for (auto& linkEntry : links.arr) {
+		linkEntries.push_back(&linkEntry);
+	}
+
+	std::vector<ParsedNode> parsedNodes(nodeEntries.size());
+
+	struct MinMax {
+		glm::vec2 min;
+		glm::vec2 max;
+	};
+
+	std::vector<MinMax> localExtremes(_threader->num_threads);
+
+	glm::vec2 minPos(FLT_MAX);
+	glm::vec2 maxPos(FLT_MIN);
+
+	if (_threader) {
+		_threader->parallel(nodeEntries.size(), [&](int start, int end) {
+			glm::vec2 local_minPos(FLT_MAX);
+			glm::vec2 local_maxPos(FLT_MIN);
+			
+			for (int i = start; i < end; i++) {
+				int nodeId = std::stoi(nodeEntries[i].first);
+				auto& nodeInfo = *nodeEntries[i].second;
+				float x = nodeInfo.obj["metadata"].obj["x"].num / 10.0f;
+				float y = nodeInfo.obj["metadata"].obj["y"].num / 10.0f;
+
+				parsedNodes[i] = { nodeId, glm::vec3(x, y, 0) };
+
+				local_minPos.x = std::min(local_minPos.x, x);
+				local_minPos.y = std::min(local_minPos.y, y);
+				local_maxPos.x = std::max(local_maxPos.x, x);
+				local_maxPos.y = std::max(local_maxPos.y, y);
+			}
+			int threadID = (start * _threader->num_threads) / nodeEntries.size();
+			localExtremes[threadID] = { local_minPos, local_maxPos };
+			});
+	}
+
+	for (const auto& mm : localExtremes) {
+		minPos.x = std::min(minPos.x, mm.min.x);
+		minPos.y = std::min(minPos.y, mm.min.y);
+		maxPos.x = std::max(maxPos.x, mm.max.x);
+		maxPos.y = std::max(maxPos.y, mm.max.y);
+	}
+
+	std::vector<ParsedLink> parsedLinks(linkEntries.size());
+
+	if (_threader) {
+		_threader->parallel(linkEntries.size(), [&](int start, int end) {
+			for (int i = start; i < end; i++) {
+				auto& linkInfo = *linkEntries[i];
+				int fromID = linkInfo.obj["source"].num;
+				int toID = linkInfo.obj["target"].num;
+
+				parsedLinks[i] = {i, fromID, toID };
+			}
+			});
+	}
+
+	std::vector<Entity*> nodeEntities;
+	nodeEntities.reserve(parsedNodes.size());
+
+	for (const auto& parsedNode : parsedNodes) {
+		auto& node(manager.addEntity<Node>());
+
+		node.addGroup(Manager::groupNodes_0);
+
+		//addNodeFunc(node, glm::vec3(x, y, z));
+		nodeEntities.push_back(&node);
+	}
+
+	if (_threader) {
+		_threader->parallel(nodeEntities.size(), [&](int start, int end) {
+			for (int i = start; i < end; i++) {
+				addNodeFunc(*nodeEntities[i], parsedNodes[i].pos);
+			}
+			});
+	}
+
+	std::vector<Entity*> linkEntities;
+	linkEntities.reserve(parsedLinks.size());
+
+	for (const auto& parsedLink : parsedLinks) {
+		auto& link = manager.addEntity<Link>(parsedLink.fromId, parsedLink.toId);
+
+		link.addGroup(Manager::groupLinks_0);
+
+		linkEntities.push_back(&link);
+	}
+
+	if (_threader) {
+		_threader->parallel(linkEntities.size(), [&](int start, int end) {
+			for (int i = start; i < end; i++) {
+				addLinkFunc(*linkEntities[i]);
+			}
+			});
+	}
 
 	float width = maxPos.x - minPos.x;
 	float height = maxPos.y - minPos.y;
