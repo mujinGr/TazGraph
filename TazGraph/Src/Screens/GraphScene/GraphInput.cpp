db9284514b69b0f74ec3c4d023472a20097dc0dc
Mujin <lefteriskotsonas2@gmail.com>
2026-01-05 18:59:20 +0200

    fixed isMouseInViewport

--- TazGraph/Src/Screens/GraphScene/GraphInput.cpp
+++ TazGraph/Src/Screens/GraphScene/GraphInput.cpp
@@ -758,6 +758,16 @@ void Graph::checkInput() {
 		break;
 		}
 	}
+	if (!_graphEditorLayer.getSubcomponent<GraphMiddlePanel>()->
+		getSubcomponent<ViewportPanel>()->
+		isMouseInSecondColumn) {
+		_isDraggingSelectionBox = false;
+		_selectionStartPos = glm::vec2(0);
+		_selectionCurrentPos = glm::vec2(0);
+		_selectionWindowStartPos = glm::vec2(0);
+		_selectionWindowCurrentPos = glm::vec2(0);
+		return;
+	}
 
 	float accelerationX = 0.0f;
 	float accelerationY = 0.0f;
